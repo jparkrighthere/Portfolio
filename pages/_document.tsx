021ba599cfba99268d715a88ea0139217c32f830
Jeonghyeon Park <jpark634@wisc.edu>
2024-02-18 03:45:37 -0600

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ const Document = () => (
   <Html>
     <Head>
       <link rel="icon" href="/favicon.ico"/>
-      <link rel="apple-touch-icon" href="/favicon.ico"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico"/>
     </Head>
     <body>
       <Main />
